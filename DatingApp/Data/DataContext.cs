bf5473639cc030a3638bd3145143f04dde7dc1fa
ozkul <rifkiozkul@gmail.com>
2019-11-09 18:04:53 -0500

    Initial Commit

---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+        public DbSet<Values> Values { get; set; }
+    }
+}
